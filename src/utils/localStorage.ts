60145cadfb449d71f74b3d5d6224f679b987a13b
Isaias Javier Ovelar <Isaiasjovelar@gmail.com>
2026-04-27 07:29:30 -0300

    Incorporadas funciones para recuperar y guardar arreglos de usuarios

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,12 +1,23 @@
 import type { IUser } from "../types/IUser";
 
-export const saveUser = (user: IUser[]) => {
+export const saveUser = (user: IUser) => {
   const parseUser = JSON.stringify(user);
   localStorage.setItem("userData", parseUser);
 };
+
+export const saveUsers = (user: IUser[]) => {
+  const parseUser = JSON.stringify(user);
+  localStorage.setItem("users", parseUser);
+};
+
 export const getUSer = () => {
   return localStorage.getItem("userData");
 };
+
+export const getUSers = () => {
+  return localStorage.getItem("users");
+};
+
 export const removeUser = () => {
   localStorage.removeItem("userData");
 };
