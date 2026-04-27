1669608d360a7484b90c36cd842e2b790c16f90a
Isaias Javier Ovelar <Isaiasjovelar@gmail.com>
2026-04-27 07:30:46 -0300

    Corregidas funciones de recuperar y almacenar credenciales de usuarios

--- src/pages/auth/registro/registro.ts
+++ src/pages/auth/registro/registro.ts
@@ -1,18 +1,17 @@
 import type { IUser } from "../../../types/IUser";
-import type { Rol } from "../../../types/Rol";
+//import type { Rol } from "../../../types/Rol";
 import { navigate } from "../../../utils/navigate";
-import { getUSer, saveUser } from "../../../utils/localStorage";
+import { getUSers,saveUsers } from "../../../utils/localStorage";
 
 const form = document.getElementById("formRegistro") as HTMLFormElement;
 const inputEmail = document.getElementById("inputEmail") as HTMLInputElement;
 const inputPassword = document.getElementById("inputPassword") as HTMLInputElement;
-const selectRol = document.getElementById("rol") as HTMLSelectElement;
 
 form.addEventListener("submit", (e: SubmitEvent) => {
   e.preventDefault();
   const valueEmail = inputEmail.value.trim();
   const valuePassword = inputPassword.value.trim();
-  const valueRol = selectRol.value as Rol;
+  const valueRol = "client";
 
   if(valueEmail && valuePassword && valueRol){
     const user: IUser = {
@@ -22,24 +21,24 @@ form.addEventListener("submit", (e: SubmitEvent) => {
     loggedIn: true,
   };
 
-    let credencialesJSON = getUSer();
+    let credencialesJSON = getUSers();
     let temp = credencialesJSON ? JSON.parse(credencialesJSON) : [];
     // Forzamos que sea un arreglo para que aparezca .length
     let credenciales = Array.isArray(temp) ? temp : [];
 
     let i=0;
-    let encontrado=0;
+    let encontrado=false;
     while( i < credenciales.length){
       if(credenciales[i].email==user.email){
         alert("La cuenta ya existe!")
-        encontrado=1;
+        encontrado=true;
       }
       i++;
     }
     
-    if(encontrado==0){
+    if(!encontrado){
       credenciales.push(user);
-      saveUser(credenciales);
+      saveUsers(credenciales);
       alert("Registro satisfactorio!")
   
       navigate("/src/pages/auth/login/login.html")
